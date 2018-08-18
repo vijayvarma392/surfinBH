85d2975e968989e81462d84e7a5b34495e223a2b
Vijay Varma <vvarma@caltech.edu>
2018-08-18 05:00:05 -0700

    Add __doc__

--- surfinBH/__init__.py
+++ surfinBH/__init__.py
@@ -1,3 +1,43 @@
+"""surfinBH
+========
+
+An to use easy interface to Surrogate Final BH properties fits.
+
+Example usage
+-------------
+
+To evaluate fits for remnant mass, spin and kick velocity of nonprecessing
+binary black hole systems as presented in Varma:2018_in_prep.
+
+import surfinBH
+
+# First load the model
+fit_3dq8 = surfinBH.LoadFits('3dq8')
+
+# Mass ratio and component spins along orbital angular momentum direction
+q = 6.7
+chi1z = 0.74
+chi2z = -0.6
+x = [q, chi1z, chi2z]
+
+## Evaluate the fits and GPR error estimate.
+
+# Final mass
+mC, mC_err_est = fit_3dq8('mC', x)
+
+# Final spin along orbital angular momentum direction
+chiCz, chiCz_err_est = fit_3dq8('chiC', x)
+
+# Final kick velocity (velCz is zero for nonprecessing systems)
+velC, velC_err_est = fit_3dq8('velC', x)
+velCx, velCy = velC
+velCx_err_est, velCy_err_est = velC_err_est
+
+Additional examples can be found in the accompanying ipython notebooks.
+To get a list of all available fits do:
+surfinBH.FIT_CLASSES.keys()
+"""
+
 __copyright__ = "Copyright (C) 2018 Vijay Varma"
 __email__ = "vvarma@caltech.edu"
 __status__ = "testing"
@@ -20,7 +60,8 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
-""" 
+"""
 
 from _loadFits import LoadFits
+from _loadFits import FIT_CLASSES
 from _downloadData import DownloadData
