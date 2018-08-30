8e8975a6b47c084db44a46290537560884bb601f
Vijay Varma <vvarma@caltech.edu>
2018-08-30 12:49:36 -0700

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="surfinBH",
-    version="0.0.7.dev1",
+    version="0.0.8.dev0",
     author="Vijay Varma",
     author_email="vvarma@caltech.edu",
     description="Surrogate Final BH properties.",
