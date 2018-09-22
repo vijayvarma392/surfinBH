5b17c474993767f02a602a5aa60dad00973d1480
Vijay Varma <vvarma@caltech.edu>
2018-09-22 12:45:24 -0700

    Clean up animation

--- animations/animation.py
+++ animations/animation.py
@@ -2,7 +2,7 @@ desc = """Animations of binary black hole scattering.
 Generates an animation of a binary black hole merger and the final remnant.
 
 Example usage:
-python animation.py --q 2 --omega0 1e-2 --chiA0 0.6 0.5 -0.1 --chiB0 0.7 0.3 0.1
+python animation.py --q 2 --omega0 1.8e-2 --chiA0 0.6 0.5 -0.1 --chiB0 0.7 0.3 0.1
 
 Note: Time values displayed in the plot are non-uniform and non-linear:
 During the inspiral there are 20 frames per orbit.
@@ -118,7 +118,9 @@ def get_quivers(Bh_loc, chi_vec, scale_factor=10):
     return [[[x, y, z], [u, v, w]] for x, y, z, u, v, w in zip(*list(segments))]
 
 #----------------------------------------------------------------------------
-def update_lines(num, lines, hist_frames):
+def update_lines(num, lines, hist_frames, t, dataLines_binary, \
+        dataLines_remnant, time_text, BhA_traj, BhB_traj, BhC_traj, \
+        chiA_nrsur, chiB_nrsur, chif, zero_idx):
     """ The function that goes into animation
     """
     current_time = t[num]
@@ -185,148 +187,151 @@ def update_lines(num, lines, hist_frames):
 
     return lines
 
-#############################    main    ##################################
 
-parser = argparse.ArgumentParser(description=desc,
-    formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument('--omega0', type=float, required=True,
-    help='Starting orbital frequency. Currently, > 0.018.')
-parser.add_argument('--q', type=float, required=True,
-    help='Mass ratio.')
-parser.add_argument('--chiA0', type=float, required=True, nargs=3,
-    help='Spin of BhA at omega0. Array of size 3.')
-parser.add_argument('--chiB0', type=float, required=True, nargs=3,
-    help='Spin of BhB at omega0. Array of size 3.')
-parser.add_argument('--fit_name', type=str, default='surfinBH7dq2', \
-    help='Fit to use.')
-
-args = parser.parse_args()
-
-q = args.q
-chiA0 = np.array(args.chiA0)
-chiB0 = np.array(args.chiB0)
-omega0 =  args.omega0
-
-# evaluate remnant fit
-fit_name = args.fit_name
-fit = surfinBH.LoadFits(fit_name)
-mf, chif, vf, mf_err, chif_err, vf_err = fit.all(q, chiA0, chiB0, omega0=omega0)
-
-mA = q/(1.+q)
-mB = 1./(1.+q)
-
-nr_sur = NRSur7dq2.NRSurrogate7dq2()
-
-# get NRSur dynamics
-quat_nrsur, orbphase_nrsur, _, _ \
-    = nr_sur.get_dynamics(q, chiA0, chiB0, omega_ref=omega0, \
-    allow_extrapolation=True)
-
-pts_per_orbit = 20
-t_binary = get_uniform_in_orbits_times(nr_sur.tds, orbphase_nrsur, \
-    pts_per_orbit)
-
-# interpolate dynamics on to t_binary
-quat_nrsur = np.array([spline_interp(t_binary, nr_sur.tds, tmp) \
-    for tmp in quat_nrsur])
-orbphase_nrsur = spline_interp(t_binary, nr_sur.tds, orbphase_nrsur)
-
-omega_nrsur = get_omegaOrb_from_sparse_data(t_binary, orbphase_nrsur)
-
-h_nrsur, chiA_nrsur, chiB_nrsur = nr_sur(q, chiA0, chiB0, \
-    f_ref=omega0/np.pi, return_spins=True, allow_extrapolation=True, LMax=2,
-    t=t_binary)
-
-#LHat = rotations.lHat_from_quat(quat_nrsur)
-
-separation = get_separation_from_omega(omega_nrsur)
-BhA_traj = get_trajectory(separation * mB, quat_nrsur, orbphase_nrsur, 'A')
-BhB_traj = get_trajectory(separation * mA, quat_nrsur, orbphase_nrsur, 'B')
-
-
-# time array for remnant
-t_remnant = np.arange(0, 10000, 100)
-
-# assume merger is at origin
-BhC_traj = np.array([tmp*t_remnant for tmp in vf])
-
-# Attaching 3D axis to the figure
-fig = P.figure()
-ax = axes3d.Axes3D(fig)
-
-# FIXME check that this makes sense
-markersize_BhA = mA*50
-markersize_BhB = mB*50
-markersize_BhC = mf*50
-
-time_text = ax.text2D(0.05, 0.9, '', transform=ax.transAxes, fontsize=14)
-
-# NOTE: Can't pass empty arrays into 3d version of plot()
-dataLines_binary = [BhA_traj, BhB_traj, BhA_traj, BhB_traj, 1, 1]
-
-lines = [\
-    # These two are for plotting component tracjectories
-    ax.plot(BhA_traj[0,0:1], BhA_traj[1,0:1], BhA_traj[2,0:1])[0], \
-    ax.plot(BhB_traj[0,0:1], BhB_traj[1,0:1], BhB_traj[2,0:1])[0], \
-
-    # These two are for plotting component BHs
-    ax.plot(BhA_traj[0,0:1], BhA_traj[1,0:1], BhA_traj[2,0:1], \
-        marker='o', markersize=markersize_BhA, markerfacecolor='k', \
-        markeredgewidth=0)[0], \
-    ax.plot(BhB_traj[0,0:1], BhB_traj[1,0:1], BhB_traj[2,0:1], \
-        marker='o', markersize=markersize_BhB, markerfacecolor='k',
-        markeredgewidth=0)[0], \
-
-    # These two are plotting component BH spins
-    ax.quiver(0,0,0,1,1,1),
-    ax.quiver(0,0,0,1,1,1),
-
-    # This is for plotting remnant BH
-    ax.plot(BhC_traj[0,0:1]-1e10, BhC_traj[1,0:1], BhC_traj[2,0:1], \
-        marker='o', markersize=markersize_BhC, markerfacecolor='k', \
-        markeredgewidth=0)[0], \
-    # This is for plotting remnant spin
-    ax.quiver(-1e10,0,0,-1e10,-1e10,-1e10),
-    ]
-
-dataLines_remnant = [BhC_traj, 1]
-
-max_range = np.nanmax(separation)
-
-# Setting the axes properties
-ax.set_xlim3d([-max_range, max_range])
-ax.set_xlabel('X')
-
-ax.set_ylim3d([-max_range, max_range])
-ax.set_ylabel('Y')
-
-ax.set_zlim3d([-max_range, max_range])
-ax.set_zlabel('Z')
-
-ax.set_title(fit_name, fontsize=16)
-
-# Creating the Animation object
-hist_frames = 15
-
-
-zero_idx = np.argmin(np.abs(t_binary))
-
-# common time array
-t = np.append(t_binary[:zero_idx], t_remnant)
-
-line_ani = animation.FuncAnimation(fig, update_lines, len(t), \
-        fargs=(lines, hist_frames), interval=50, blit=False, repeat=True,
-        repeat_delay=5e3)
-
-# Pause settings
+#----------------------------------------------------------------------------
 pause = False
-def onClick(event):
-    global pause
-    if pause:
-        line_ani.event_source.start()
-        pause = False
-    else:
-        line_ani.event_source.stop()
-        pause = True
-fig.canvas.mpl_connect('button_press_event', onClick)
-P.show()
+def BBH_scattering(q, chiA0, chiB0, omega0):
+
+    # evaluate remnant fit
+    fit_name = 'surfinBH7dq2'
+    fit = surfinBH.LoadFits(fit_name)
+    mf, chif, vf, mf_err, chif_err, vf_err \
+        = fit.all(q, chiA0, chiB0, omega0=omega0)
+    
+    mA = q/(1.+q)
+    mB = 1./(1.+q)
+    
+    nr_sur = NRSur7dq2.NRSurrogate7dq2()
+    
+    # get NRSur dynamics
+    quat_nrsur, orbphase_nrsur, _, _ \
+        = nr_sur.get_dynamics(q, chiA0, chiB0, omega_ref=omega0, \
+        allow_extrapolation=True)
+    
+    pts_per_orbit = 20
+    t_binary = get_uniform_in_orbits_times(nr_sur.tds, orbphase_nrsur, \
+        pts_per_orbit)
+    
+    # interpolate dynamics on to t_binary
+    quat_nrsur = np.array([spline_interp(t_binary, nr_sur.tds, tmp) \
+        for tmp in quat_nrsur])
+    orbphase_nrsur = spline_interp(t_binary, nr_sur.tds, orbphase_nrsur)
+    
+    omega_nrsur = get_omegaOrb_from_sparse_data(t_binary, orbphase_nrsur)
+    
+    h_nrsur, chiA_nrsur, chiB_nrsur = nr_sur(q, chiA0, chiB0, \
+        f_ref=omega0/np.pi, return_spins=True, allow_extrapolation=True, LMax=2,
+        t=t_binary)
+    
+    #LHat = rotations.lHat_from_quat(quat_nrsur)
+    
+    separation = get_separation_from_omega(omega_nrsur)
+    BhA_traj = get_trajectory(separation * mB, quat_nrsur, orbphase_nrsur, 'A')
+    BhB_traj = get_trajectory(separation * mA, quat_nrsur, orbphase_nrsur, 'B')
+    
+    
+    # time array for remnant
+    t_remnant = np.arange(0, 10000, 100)
+    
+    # assume merger is at origin
+    BhC_traj = np.array([tmp*t_remnant for tmp in vf])
+    
+    # Attaching 3D axis to the figure
+    fig = P.figure()
+    ax = axes3d.Axes3D(fig)
+    
+    # FIXME check that this makes sense
+    markersize_BhA = mA*50
+    markersize_BhB = mB*50
+    markersize_BhC = mf*50
+    
+    time_text = ax.text2D(0.05, 0.9, '', transform=ax.transAxes, fontsize=14)
+    
+    # NOTE: Can't pass empty arrays into 3d version of plot()
+    dataLines_binary = [BhA_traj, BhB_traj, BhA_traj, BhB_traj, 1, 1]
+    
+    lines = [\
+        # These two are for plotting component tracjectories
+        ax.plot(BhA_traj[0,0:1], BhA_traj[1,0:1], BhA_traj[2,0:1])[0], \
+        ax.plot(BhB_traj[0,0:1], BhB_traj[1,0:1], BhB_traj[2,0:1])[0], \
+    
+        # These two are for plotting component BHs
+        ax.plot(BhA_traj[0,0:1], BhA_traj[1,0:1], BhA_traj[2,0:1], \
+            marker='o', markersize=markersize_BhA, markerfacecolor='k', \
+            markeredgewidth=0)[0], \
+        ax.plot(BhB_traj[0,0:1], BhB_traj[1,0:1], BhB_traj[2,0:1], \
+            marker='o', markersize=markersize_BhB, markerfacecolor='k',
+            markeredgewidth=0)[0], \
+    
+        # These two are plotting component BH spins
+        ax.quiver(0,0,0,1,1,1),
+        ax.quiver(0,0,0,1,1,1),
+    
+        # This is for plotting remnant BH
+        ax.plot(BhC_traj[0,0:1]-1e10, BhC_traj[1,0:1], BhC_traj[2,0:1], \
+            marker='o', markersize=markersize_BhC, markerfacecolor='k', \
+            markeredgewidth=0)[0], \
+        # This is for plotting remnant spin
+        ax.quiver(-1e10,0,0,-1e10,-1e10,-1e10),
+        ]
+    
+    dataLines_remnant = [BhC_traj, 1]
+    
+    max_range = np.nanmax(separation)
+    
+    # Setting the axes properties
+    ax.set_xlim3d([-max_range, max_range])
+    ax.set_xlabel('X')
+    
+    ax.set_ylim3d([-max_range, max_range])
+    ax.set_ylabel('Y')
+    
+    ax.set_zlim3d([-max_range, max_range])
+    ax.set_zlabel('Z')
+    
+    ax.set_title(fit_name, fontsize=16)
+    
+    # Creating the Animation object
+    hist_frames = 15
+    
+    
+    zero_idx = np.argmin(np.abs(t_binary))
+    
+    # common time array
+    t = np.append(t_binary[:zero_idx], t_remnant)
+    
+    line_ani = animation.FuncAnimation(fig, update_lines, len(t), \
+        fargs=(lines, hist_frames, t, dataLines_binary, dataLines_remnant, \
+            time_text, BhA_traj, BhB_traj, BhC_traj, chiA_nrsur, chiB_nrsur, \
+            chif, zero_idx), \
+        interval=50, blit=False, repeat=True, repeat_delay=5e3)
+    
+    # Pause settings
+    def onClick(event):
+        global pause
+        if pause:
+            line_ani.event_source.start()
+            pause = False
+        else:
+            line_ani.event_source.stop()
+            pause = True
+    fig.canvas.mpl_connect('button_press_event', onClick)
+    P.show()
+
+#############################    main    ##################################
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description=desc,
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--omega0', type=float, required=True,
+        help='Starting orbital frequency. Currently, > 0.018.')
+    parser.add_argument('--q', type=float, required=True,
+        help='Mass ratio.')
+    parser.add_argument('--chiA0', type=float, required=True, nargs=3,
+        help='Spin of BhA at omega0. Array of size 3.')
+    parser.add_argument('--chiB0', type=float, required=True, nargs=3,
+        help='Spin of BhB at omega0. Array of size 3.')
+    
+    args = parser.parse_args()
+    BBH_scattering(args.q, np.array(args.chiA0), np.array(args.chiB0), \
+        args.omega0)
