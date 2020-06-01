823b5fa9467500e77a7b81732ec3083a0a8b0ea6
Vijay Varma <vvarma@caltech.edu>
2020-06-01 14:44:17 -0700

    Set max version for scikit-learn
    They broke some backward compatibility, sigh..

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setuptools.setup(
         'numpy',
         'scipy',
         'h5py',
-        'scikit-learn>=0.19.1',
+        'scikit-learn>=0.19.1,<0.23.1',
         'lalsuite>=6.70',
         'gwsurrogate',
     ],
